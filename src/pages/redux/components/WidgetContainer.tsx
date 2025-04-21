14b2eb1c42d37f55e4fe9495e3cb49f68def61b6
ChiragChrg <chiruchirag2001@gmail.com>
2025-04-21 14:07:02 +0530

    ✨ Update WidgetContainer components to reduce update interval from 50ms to 17ms for improved performance and reduce re-renders

--- src/pages/redux/components/WidgetContainer.tsx
+++ src/pages/redux/components/WidgetContainer.tsx
@@ -10,13 +10,13 @@ const WidgetContainer: React.FC = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       dispatch(widgetActions.updateWidget());
-    }, 50);
+    }, 17);
 
     return () => clearInterval(interval);
   }, [dispatch]);
 
   return <div className="border-t border-slate-400">
-    <p className='text-center !py-4'>1000 widgets are updated every 50ms with Redux which causes re-renders when accessing the updated state using useSelector() hook</p>
+    <p className='text-center !py-4'>1000 widgets are updated every 17ms with Redux which causes re-renders when accessing the updated state using useSelector() hook</p>
     <div className="w-full grid grid-cols-6 lg:grid-cols-10 gap-8 !p-4">
       {_.range(0, 1000).map((_, index) => (
         <Widget key={index} />
--- src/pages/signal/components/WidgetContainer.tsx
+++ src/pages/signal/components/WidgetContainer.tsx
@@ -6,12 +6,12 @@ import _ from 'lodash';
 
 const WidgetContainer: React.FC = () => {
   effect(() => {
-    const interval = setInterval(updateWidget, 50);
+    const interval = setInterval(updateWidget, 17);
     return () => clearInterval(interval);
   });
 
   return <div className="border-t border-slate-400">
-    <p className='text-center !py-4'>1000 widgets are updated every 50ms with Signals resulting in ZERO re-renders by directly painting the DOM using Refs</p>
+    <p className='text-center !py-4'>1000 widgets are updated every 17ms with Signals resulting in ZERO re-renders by directly painting the DOM using Refs</p>
     <div className="w-full grid grid-cols-6 lg:grid-cols-10 gap-8 !p-4">
       {_.range(0, 1000).map((_, index) => (
         <Widget key={index} />
