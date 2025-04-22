5d3cb6455efca334e5bb94915596744ca003262f
ChiragChrg <chiruchirag2001@gmail.com>
2025-04-22 10:48:29 +0530

    ✨ Refactor Widget components to improve rendering efficiency; update styling and messaging in WidgetContainer

--- src/pages/redux/components/Widget.tsx
+++ src/pages/redux/components/Widget.tsx
@@ -1,33 +1,27 @@
-import React, { useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux';
 import { SEL_Widget } from '../../../store/redux';
+import { useEffect, useRef } from 'react';
 
 const Widget: React.FC = () => {
-    const barRef = useRef<HTMLDivElement>(null)
-    const valueRef = useRef<HTMLSpanElement>(null)
-
+    const barRef = useRef<HTMLDivElement>(null);
     const value = useSelector(SEL_Widget); // This will cause re-render
-    console.log("Render Widget")
+    console.log("Render Redux Widget")
 
     useEffect(() => {
         if (barRef.current) {
-            barRef.current.style.width = `${value}%`
-        }
-        if (valueRef.current) {
-            valueRef.current.textContent = `${value}%`
+            barRef.current.style.width = `${value}%`;
         }
     }, [value])
 
     return <div className="flex gap-1 w-full h-fit bg-slate-700">
         <div className="w-full bg-slate-500">
             <div
-                ref={barRef}
                 className="h-full bg-amber-400"
-                style={{ width: `$0%` }}
+                style={{ width: `${value}%` }}
             ></div>
         </div>
 
-        <span ref={valueRef} className="w-[6ch] text-white text-center">0</span>
+        <span className="w-[6ch] text-white text-center">{value}</span>
     </div>
 }
 
--- src/pages/signal/components/Widget.tsx
+++ src/pages/signal/components/Widget.tsx
@@ -1,21 +1,16 @@
-import React, { useRef } from 'react'
+import { useRef } from 'react';
 import { widgetSignal } from '../../../store/signal'
 import { effect } from '@preact/signals-react';
 
 const Widget: React.FC = () => {
-    const barRef = useRef<HTMLDivElement>(null)
-    const valueRef = useRef<HTMLSpanElement>(null)
-    console.log("Render Widget")
+    const barRef = useRef<HTMLDivElement>(null);
+    console.log("Render Signal Widget")
 
     effect(() => {
         const value = widgetSignal.value
 
-        // console.log("Widget effect", value)
         if (barRef.current) {
-            barRef.current.style.width = `${value}%`
-        }
-        if (valueRef.current) {
-            valueRef.current.textContent = `${value}%`
+            barRef.current.style.width = `${value}%`;
         }
     })
 
@@ -28,7 +23,7 @@ const Widget: React.FC = () => {
             ></div>
         </div>
 
-        <span ref={valueRef} className="w-[6ch] text-white text-center">0</span>
+        <span className="w-[6ch] text-white text-center">{widgetSignal}</span>
     </div>
 }
 
